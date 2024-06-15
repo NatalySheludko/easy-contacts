cc68e79c4dd2fb6ff617dbc1da1a1b80971985d9
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:52:04 +0300

    add SearchBox component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,32 +1,58 @@
+import css from "./SearchBox.module.css";
+
 import { useDispatch, useSelector } from "react-redux";
-import css from "../SearchBox/SearchBox.module.css";
-import { changeFilter } from "../../redux/filtersSlice.js";
-import { selectNameFilter } from "../../redux/filtersSlice.js";
 import { useId } from "react";
 
+import { changeFilter } from "../../redux/filters/slice.js";
+
+import {
+  selectNameFilter,
+  selectNumberFilter,
+} from "../../redux/filters/selectors.js";
+
+import { RiFileSearchFill } from "react-icons/ri";
+import { IconContext } from "react-icons";
+
+
+
 export default function SearchBox() {
-  const fieldId = useId();
+	const fieldId = useId();
+	
   const dispatch = useDispatch();
 
   const name = useSelector(selectNameFilter);
+  const number = useSelector(selectNumberFilter);
 
-  const handleFindName = (event) => {
-    dispatch(changeFilter(event.target.value.toLowerCase()));
-	};
-	
+  const handleFindName = (e) => {
+    const value = e.target.value;
+    dispatch(
+      changeFilter({
+        name: value.toLowerCase(),
+        number: value,
+      })
+    );
+  };
 
   return (
-    <div className={css.filter}>
-      <label className={css.inputLabel} htmlFor={`${fieldId}-name`}>
-        Find contacts by name
-      </label>
-      <input
-        className={css.input}
-        id={`${fieldId}-name`}
-        type="text"
-        value={name}
-        onChange={handleFindName}
-      />
+    <div className={css.container}>
+      <div className={css.formContainer}>
+        <label className={css.inputLabel} htmlFor={`${fieldId}-name`}>
+        </label>
+        <div className={css.iconPosition}>
+          <input
+            className={css.input}
+            id={`${fieldId}-name`}
+            type="text"
+            value={name || number}
+            onChange={handleFindName}
+          />
+          <IconContext.Provider value={{ size: "2em" }}>
+            <span className={css.inputIcon}>
+              <RiFileSearchFill />
+            </span>
+          </IconContext.Provider>
+        </div>
+      </div>
     </div>
   );
 }
