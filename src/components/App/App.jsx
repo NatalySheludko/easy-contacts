d6de14dcad63fad03a6670a48ba025c293cd611e
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 21:28:40 +0300

    create app component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,76 @@
-import ContactForm from "../ContactForm/ContactForm";
-import ContactList from "../ContactList/ContactList";
-import SearchBox from "../SearchBox/SearchBox";
 import css from "../App/App.module.css";
+
+import { lazy, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { selectError, selectLoading } from "../../redux/contactsSlice";
-import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contactsOps";
+
+import { selectRefreshing } from "../../redux/auth/selectors";
+import { refreshUser } from "../../redux/auth/operations";
+
+import { RestrictedRoute } from "../RestrictedRoute/RestrictedRoute";
+import { Layout } from "../Layout/Layout";
+import { PrivateRoute } from "../PrivateRoute/PrivateRoute";
 import Loading from "../Loading/Loading";
-import MessageError from "../MessageError/MessageError";
+import NotificationToast from "../NotificationToast/NotificationToast";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const RegistrationPage = lazy(() =>
+  import("../../pages/RegistrationPage/RegistrationPage")
+);
+const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() =>
+  import("../../pages/ContactsPage/ContactsPage")
+);
+const NotFoundPage = lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage")
+);
+
+
 
 export default function App() {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectLoading);
-  const isError = useSelector(selectError);
+	const dispatch = useDispatch();
+	
+  const { isRefreshing } = useSelector(selectRefreshing);
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
-    <div>
-      <h1 className={css.app}>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      {isError && <MessageError>Error message!</MessageError>}
-      {isLoading && <Loading>Request in progress...</Loading>}
-      <ContactList />
+  return isRefreshing ? (
+    <Loading />
+  ) : (
+    <div className={css.app}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<RegistrationPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                component={<LoginPage />}
+                redirectTo="/contacts"
+              />
+            }
+          />
+          <Route
+            path="contacts/"
+            element={
+              <PrivateRoute component={<ContactsPage />} redirectTo="/login" />
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
+      <NotificationToast />
     </div>
   );
 }
