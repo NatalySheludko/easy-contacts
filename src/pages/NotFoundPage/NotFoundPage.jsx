471e41090f79c1f657edbbcf55be1fa56e011cf4
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:51:07 +0300

    add NotFoundPage component

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import css from "../NotFoundPage/NotFoundPage.module.css";
+
+import { Link } from "react-router-dom";
+
+
+
+export default function NotFoundPage() {
+	
+  return (
+    <p className={css.text}>
+      Sorry, page not found! Please go to <Link to="/" className={css.link}> 
+        Home Page!
+      </Link>      
+    </p>
+  );
+}
