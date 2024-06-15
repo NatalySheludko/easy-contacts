c1915e2d50fb43808d5e226f450556b9033ae409
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:50:31 +0300

    add auth selectors

---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,9 @@
+export const selectLoggedIn = (state) => state.auth.isLoggedIn;
+
+export const selectUser = (state) => state.auth.user;
+
+export const selectRefreshing = (state) => state.auth.isRefreshing;
+
+export const selectLoading = (state) => state.auth.loading;
+
+export const selectError = (state) => state.auth.error;
