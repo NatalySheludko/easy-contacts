57eeb960b7ac8ec1c9af3e676dda05110ae3ed67
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:50:09 +0300

    add contacts operations

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get("/contacts");
+      return res.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (items, thunkAPI) => {
+    try {
+      const res = await axios.post("/contacts", items);
+      return res.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (itemId, thunkAPI) => {
+    try {
+      const res = await axios.delete(`/contacts/${itemId}`);
+      return res.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const patchContact = createAsyncThunk(
+  "contacts/patchContact",
+  async (updatedContact, thunkAPI) => {
+    const { itemId, name, number } = updatedContact;
+    try {
+      const res = await axios.patch(`/contacts/${itemId}`, { name, number });
+      return res.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
