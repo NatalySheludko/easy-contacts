74b146c87fad6b0b1237d39ed4a24c079be02fa1
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:47:10 +0300

    add Persist/BrowserRoute

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import App from "./components/App/App";
 import "./index.css";
 import "modern-normalize";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";//
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
