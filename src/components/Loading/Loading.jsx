60c69a18ebecbcb56cbc932b2aa2bb26f3864246
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:45:25 +0300

    create Loader component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,5 +1,22 @@
 import css from "../Loading/Loading.module.css";
 
-export default function Loading({ children }) {
-  return <p className={css.textLoader}>{children}</p>;
+import { Comment } from "react-loader-spinner";
+
+
+
+export default function Loading() {
+  return (
+    <div className={css.loader}>
+      <Comment
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="comment-loading"
+        wrapperStyle={{}}
+        wrapperClass="comment-wrapper"
+        color="#fff"
+        backgroundColor="#F4442E"
+      />
+    </div>
+  );
 }
