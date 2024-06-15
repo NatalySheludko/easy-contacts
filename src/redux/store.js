3920a20bf5522d2f7067ddd27d5cc620877e175a
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:48:16 +0300

    update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,51 @@
 import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from "./contactsSlice";
-import filtersReducer from "./filtersSlice";
+import { contactsReducer } from "./contacts/slice";
+import filtersReducer from "./filters/slice";
+import { authReducer } from "./auth/slice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+
+const persistedContactsReducer = persistReducer(
+  {
+    key: "contactsValue",
+    storage,
+    whitelist: ["items"],
+  },
+  contactsReducer
+);
+
+const persistedAuthReducer = persistReducer(
+  {
+    key: "auth",
+    storage,
+    whitelist: ["token"],
+  },
+  authReducer
+);
+
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
     filters: filtersReducer,
+    auth: persistedAuthReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
