ef67619cfcce0a91b1bed989e64ca76dede13958
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-15 23:51:31 +0300

    add HomePage component

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import css from "../HomePage/HomePage.module.css";
+
+
+
+export default function HomePage() {
+
+  return (
+    <div className={css.homePage}>
+      <h1 className={css.title}>Task manager welcome page ⭐</h1>
+    </div>
+  );
+}
